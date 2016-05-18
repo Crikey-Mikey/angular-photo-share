be225564767a6afb357284bae74abd994d54311b
Mike Lewis <mick.lewis84@gmail.com>
2016-05-18 23:09:15 +1000

    Add bootswatch theme, navbar and gulp changes

--- gulp/build.js
+++ gulp/build.js
@@ -71,10 +71,12 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+  return gulp.src([
+    'bower_components/bootstrap-sass/assets/fonts/**/*',
+    'bower_components/font-awesome/fonts/*',
+  ])
+  .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+  .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
 gulp.task('other', function () {
